a5b51d1c4ed23068d3fffd3a23921102697a8139
cwbirkenshaw-svg <cwbirkenshaw@gmail.com>
2026-03-16 13:16:48 +0000

    Delete data/rituals directory

--- data/rituals/index.ts
+++
@@ -1,8 +0,0 @@
-import { RitualSection } from '@/types';
-import { NORTHUMBERLAND_RITUALS } from './northumberland';
-import { EMULATION_RITUALS } from './emulation';
-
-export const SAMPLE_RITUALS: RitualSection[] = [
-  ...NORTHUMBERLAND_RITUALS,
-  ...EMULATION_RITUALS,
-];
